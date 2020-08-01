ebc859771c42603d9b8a46a541fb9ba91224d2ca
Nouani <nouanigabriel@hotmail.com>
2020-07-31 23:56:13 -0300

    /api => Remoção da matricula e cadastro do resultado + validações

--- api/src/index.js
+++ api/src/index.js
@@ -15,6 +15,31 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.post('/resultados', async (req, res) => {
+    const { ra, codDisciplina, nota, frequencia } = req.body;
+
+    const alunoExists = await execSQLQuery(`select * from Alunos where RA = ${ra}`);
+    if (alunoExists.recordset.length === 0) {
+        return res.status(400).json({ error: 'RA inválido' })
+    }
+
+    const disciplinaExists = await execSQLQuery(`select * from Disciplinas where cod = ${codDisciplina}`);
+    if (disciplinaExists.recordset.length === 0) {
+        return res.status(400).json({ error: 'Código de disciplina inválido' })
+    }
+
+    const matriculaExists = await execSQLQuery(`select * from Matriculas where RA = ${ra} and cod = ${codDisciplina}`);
+    if (matriculaExists.recordset.length === 0) {
+        return res.status(404).json({ error: `O aluno de RA: ${ra} não está matriculado na disciplina de código: ${codDisciplina}` });
+    }
+
+    await execSQLQuery(`delete from Matriculas where RA = ${ra} and cod = ${codDisciplina}`);
+
+    await execSQLQuery(`insert into Resultados values (${ra}, ${codDisciplina}, ${nota}, ${frequencia})`);
+
+    return res.json();
+});
+
 app.listen('3333', () => {
     console.log('Started!');
 });

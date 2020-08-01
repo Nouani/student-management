bd26db9c86037947371921df70fe3fc3714c92b6
Nouani <nouanigabriel@hotmail.com>
2020-07-31 23:21:37 -0300

    E:/Arquivos de Programas/Git/api => Conexão com DB

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mssql = require('mssql');
+
+mssql.connect("Server=regulus.cotuca.unicamp.br;Database=BD19174;User Id=BD19174;Password=guilhermepoder;")
+    .then(conn => global.conn = conn)
+    .catch(err => console.log(err));
+
+function execSQLQuery(sqlQry) {
+    return global.conn.request().query(sqlQry);
+}
 
 const app = express();
 
